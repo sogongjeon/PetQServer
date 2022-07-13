ea568ca6feecec6db2e3b8a14292abecd17ff311
dajung <dajung_ju@hancom.com>
2022-07-13 23:12:30 +0900

    gitignore add

--- 1_build.sh
+++
@@ -1,7 +0,0 @@
-#/bin/sh
-ID=codejune
-REPOSITORY=petq-apiserver
-VERSION=v0.1
-
-docker build -t $ID/$REPOSITORY:$VERSION .
-docker push $ID/$REPOSITORY:$VERSION
--- 4_log.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-POD_NAME=petq-apiserver
-NAME=$(kubectl get pod | grep -E $POD_NAME | awk '{print $1}')
-
-kubectl logs -f $NAME
